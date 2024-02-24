22fea1494b5f5b1e14340d11cebefb981ec035c8
igorgardzielewski <igorgardzielewski3@gmail.com>
2024-02-24 02:19:45 +0100

    Initial Commit

---
+++ tess/tessApp.swift
@@ -0,0 +1,17 @@
+//
+//  tessApp.swift
+//  tess
+//
+//  Created by Igor Gardzielewski on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct tessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
